21b0590319ead7e77b373ceec0221ef98a957af3
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2017-01-03 22:53:19 -0800

    Next couple chapers

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,61 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Dustin on 2016-12-26.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.forecast_list_item, parent, false);
+
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        holder.mWeatherTextView.setText(mWeatherData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null) {
+            return 0;
+        } else {
+            return mWeatherData.length;
+        }
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View itemView) {
+            super(itemView);
+            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+        }
+    }
+}
